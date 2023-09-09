b180fc55299add96ffff7a14b34cc714f6b89045
Kamil Gadek <kamil.gadek124@gmail.com>
2023-09-09 12:47:49 +0200

    Add error handling

--- lib/app/login/login_page.dart
+++ lib/app/login/login_page.dart
@@ -1,13 +1,20 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   LoginPage({
     super.key,
   });
 
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  var ErrorMessage = '';
 
   @override
   Widget build(BuildContext context) {
@@ -21,24 +28,28 @@ class LoginPage extends StatelessWidget {
               const Text('Zaloguj się'),
               const SizedBox(height: 20),
               TextField(
-                controller: emailController,
+                controller: widget.emailController,
                 decoration: const InputDecoration(hintText: 'E-mail'),
               ),
               TextField(
-                controller: passwordController,
+                controller: widget.passwordController,
                 obscureText: true,
                 decoration: const InputDecoration(hintText: 'Hasło'),
               ),
+              const SizedBox(height: 20),
+              Text(ErrorMessage),
               const SizedBox(height: 20),
               ElevatedButton(
                 onPressed: () async {
                   try {
                     await FirebaseAuth.instance.signInWithEmailAndPassword(
-                      email: emailController.text,
-                      password: passwordController.text,
+                      email: widget.emailController.text,
+                      password: widget.passwordController.text,
                     );
                   } catch (error) {
-                    print(error);
+                    setState(() {
+                      ErrorMessage = error.toString();
+                    });
                   }
                 },
                 child: const Text('Zaloguj się'),

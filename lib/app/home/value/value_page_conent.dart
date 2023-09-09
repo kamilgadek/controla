10c9c2ca010c3a7ef0d45b9019443cb064c7f031
Kamil Gadek <kamil.gadek124@gmail.com>
2023-09-09 17:46:39 +0200

    Implement adding orders

--- lib/app/home/value/value_page_conent.dart
+++ lib/app/home/value/value_page_conent.dart
@@ -1,15 +1,58 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class ValuePageContent extends StatelessWidget {
+class ValuePageContent extends StatefulWidget {
   const ValuePageContent({
     super.key,
   });
 
   @override
+  State<ValuePageContent> createState() => _ValuePageContentState();
+}
+
+class _ValuePageContentState extends State<ValuePageContent> {
+  var contrahentsName = '';
+  var materialName = '';
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Dwa'),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextField(
+            decoration: const InputDecoration(
+              hintText: 'Podaj nazwę kontrahenta',
+            ),
+            onChanged: (newValue) {
+              setState(() {
+                contrahentsName = newValue;
+              });
+            },
+          ),
+          TextField(
+            decoration: const InputDecoration(
+              hintText: 'Podaj nazwę materiału',
+            ),
+            onChanged: (newValue) {
+              setState(() {
+                materialName = newValue;
+              });
+            },
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              FirebaseFirestore.instance.collection('contrahents').add({
+                'name' : contrahentsName,
+                'material' : materialName,
+                'value' : 4.0,
+              });
+            },
+            child: const Text('Dodaj'),
+          ),
+        ],
+      ),
     );
   }
 }
-

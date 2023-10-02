e62155b39734398bdf6c2341da258bc0af34bf2e
Kamil Gadek <kamil.gadek124@gmail.com>
2023-10-02 19:02:00 +0200

    Add BlodProvider and BlocBuilder

---
+++ lib/app/home/contrahents/cubit/contrahents_state.dart
@@ -0,0 +1,13 @@
+part of 'contrahents_cubit.dart';
+
+@immutable
+class ContrahentsState {
+  final List<QueryDocumentSnapshot<Map<String, dynamic>>> documents;
+  final bool isLoading;
+  final String errorMessage;
+
+  const ContrahentsState(
+      {required this.documents,
+      required this.isLoading,
+      required this.errorMessage});
+}
